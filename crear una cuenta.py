78d825fc0a801dbaee64dddaeb0240eb2775150d
matiunitu <114375495+matiunitu@users.noreply.github.com>
2023-09-07 17:07:13 -0500

    Add files via upload

---
+++ crear una cuenta.py
@@ -0,0 +1,6 @@
+nombre = input("cual es tu nombre: ")
+print("hola "+ nombre + ",vamos a realizar una suma")
+a = int(input("introduce el primer numero: "))
+b = int(input("intrdouce el segundo numero: "))
+resultado = a + b
+print( nombre + "el resultado de la sumaes: ", resultado)
---
+++ pitagoras.py
@@ -0,0 +1,10 @@
+print("teoremas de pitagoras")
+from math import sqrt 
+a=float(input("ingresa el lado a: "))
+b=float(input("ingresa el lado b: "))
+resultado = a**2 + b**2
+h = sqrt (resultado) 
+print("la hipotenusa es: ", h)
+
+
+
